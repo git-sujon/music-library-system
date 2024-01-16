b8cc4ba8700f27c3e451c70efe09f4b883152337
git-sujon <git.sujon@gmail.com>
2024-01-16 13:34:11 +0600

    app.ts, server.ts , middlewaeres, routes interface and config add

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import routers from './app/routes';
+import httpStatus from 'http-status';
+import globalErrorHandler from './app/middleware/globalErrorHandler';
+
+const app = express();
+
+// Configure CORS
+const allowedOrigins = ['http://localhost:3000', 'vercel live site']; 
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // Allow credentials (cookies)
+  }),
+);
+
+app.use(cookieParser());
+//  parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// application routes
+app.use('/api/v1/', routers);
+
+// global Error Handler
+app.use(globalErrorHandler);
+
+// api not found Error handler
+app.use((req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'NOT FOUND',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API NOT FOUND',
+      },
+    ],
+  });
+});
+
+export default app;
